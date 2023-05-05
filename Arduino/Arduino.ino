1c3d81e76147c173b181b0a43409424587f07afa
jean_martins <jean.carlos.martins00@gmail.com>
2023-05-04 23:35:28 -0300

    added arduino code

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,34 @@
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+void setup()
+{
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+  
+    String serialMessage = Serial.readStringUntil('\n');
+    if(serialMessage != ""){
+    String memUsage = serialMessage.substring(serialMessage.indexOf(";") + 1, serialMessage.length()); 
+    String cpuUsage = serialMessage.substring(0, serialMessage.indexOf(";")); 
+    if (cpuUsage.toInt() > 40) {
+      lcd.setRGB(255, 0, 0);
+    } else if (cpuUsage.toInt() > 20 ) {
+      lcd.setRGB(255, 255, 0);
+    } else {
+      lcd.setRGB(255, 255, 255);
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("CPU Usage: " + String(cpuUsage) + "%");
+    lcd.setCursor(0, 1);
+    lcd.print("RAM Usage: " + String(memUsage) + "%");
+    }
+  }
+}
